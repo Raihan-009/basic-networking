24a4a3e81c70b730a2f14c060f45b77cf4069dab
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-04 20:45:37 +0600

    server-client connection has been updated

--- socket/single-client/client.py
+++ socket/single-client/client.py
@@ -1,3 +1,4 @@
+from http import client
 import socket
 
 s = socket.socket()
@@ -6,3 +7,13 @@ s.connect((socket.gethostname(), 1809))
 
 server_msg = s.recv(1024)
 print(f'Message from Server :: {server_msg.decode("utf-8")}')
+ 
+conn = True
+while conn:
+    client_msg = input()
+    if client_msg == "terminate":
+        s.send(client_msg.encode("utf-8"))
+        conn = False
+        s.close()
+    else:
+        s.send(client_msg.encode("utf-8"))
--- socket/single-client/server.py
+++ socket/single-client/server.py
@@ -7,9 +7,18 @@ s = socket.socket()
 s.bind((socket.gethostname(), 1809))
 s.listen(5)
 
+client_socket, address = s.accept()
+print(f'Connection from {address} has been established!')
+client_socket.send(bytes("Hola! I am from the server", "utf-8"))
 
-while True:
-    client_socket, address = s.accept()
-    print(f'Connection from {address} has been established!')
-    client_socket.send(bytes("Hola! I am from the server", "utf-8"))
-    client_socket.close()
+conn = True
+
+while conn:
+    client_msg = client_socket.recv(1024)
+    client_msg = client_msg.decode("utf-8")
+    if (client_msg == "terminate"):
+        conn = False
+        print("Message from client :: Connection with client got disconnected")
+        s.close()
+    else:
+        print(f"Message from client :: {client_msg}")
