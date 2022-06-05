443dac924589351522b9921fad0f17457ce728de
Raihan Islam <u1809009@student.cuet.ac.bd>
2022-06-05 19:35:01 +0600

    multiple client handler server

---
+++ socket/multiple-client/server.py
@@ -0,0 +1,36 @@
+import os
+import socket
+
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+REQUEST_QUEUE_SIZE = 5
+
+
+def handle_request(client_connection):
+    request = client_connection.recv(1024)
+    http_response = "This one from the server side"
+    client_connection.send(bytes(http_response, "utf-8"))
+
+
+def serve_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind(SERVER_ADDRESS)
+    listen_socket.listen(REQUEST_QUEUE_SIZE)
+    print('Serving HTTP on port {port} ...'.format(port=PORT))
+
+    clients = []
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        clients.append(client_connection)
+        pid = os.fork()
+        if pid == 0:  # child
+            listen_socket.close()  # close child copy
+            handle_request(client_connection)
+            client_connection.close()
+            os._exit(0)  # child exits here
+        else:  # parent
+            print(len(clients))
+            client_connection.close()
+
+if __name__ == '__main__':
+    serve_forever()
